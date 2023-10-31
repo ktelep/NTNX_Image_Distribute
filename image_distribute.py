4c9646da8436b482daceffd7fa9c721baf043049
Kurt Telep <ktelep@gmail.com>
2023-10-31 11:12:20 -0400

    Updated Output Reporting

--- image_distribute.py
+++ image_distribute.py
@@ -7,7 +7,6 @@ import os
 from base64 import b64encode
 from tqdm import tqdm
 from tqdm.utils import CallbackIOWrapper
-from collections import namedtuple
 
 """ 
 Parameters
@@ -56,24 +55,31 @@ class Prism_Element:
 
     def snap_vm(self, vm_uuid, snapshot_name):
         snap_payload_template = ''' { "snapshot_specs" : [ {"snapshot_name": "TBD", "vm_uuid": "TBD" }]} '''
-        snap_payload = json.loads(snap_payload_template)
+        payload = json.loads(snap_payload_template)
 
-        snap_payload['snapshot_specs'][0]['snapshot_name'] = snapshot_name
-        snap_payload['snapshot_specs'][0]['vm_uuid'] = vm_uuid
-        payload = json.dumps(snap_payload)
+        payload['snapshot_specs'][0]['snapshot_name'] = snapshot_name
+        payload['snapshot_specs'][0]['vm_uuid'] = vm_uuid
+
         res = self._post_request('snapshots',payload)
         return res
 
     def _post_request(self, url_target, pythonic_payload):
         url = self.url + url_target
         payload = json.dumps(pythonic_payload)
-        response = requests.request('post', url, data=payload, headers=self.headers, verify=False)
+        try:
+            response = requests.request('post', url, data=payload, headers=self.headers, verify=False)
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)
         res = response.json()
         return res
     
     def _get_request(self, url_target):
         url = self.url + url_target
-        response = requests.request('get', url, headers=self.headers, verify=False)
+        try:
+            response = requests.request('get', url, headers=self.headers, verify=False)
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)   
+
         res = response.json()
         return res
 
@@ -158,13 +164,19 @@ class Prism_Central:
     def _post_request(self, url_target, pythonic_payload):
         url = self.url + url_target
         payload = json.dumps(pythonic_payload)
-        response = requests.request('post', url, data=payload, headers=self.headers, verify=False)
+        try:
+            response = requests.request('post', url, data=payload, headers=self.headers, verify=False)
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)
         res = response.json()
         return res
     
     def _get_request(self, url_target):
         url = self.url + url_target
-        response = requests.request('get', url, headers=self.headers, verify=False)
+        try:
+            response = requests.request('get', url, headers=self.headers, verify=False)
+        except requests.exceptions.RequestException as e:  
+            raise SystemExit(e)
         res = response.json()
         return res
 
@@ -216,11 +228,9 @@ class Prism_Central:
         return vm
 
     def vm_by_uuid(self, vm_uuid):
-        name_filter = f"vm_uuid=={vm_uuid}"
-        payload = {"kind":"vm","filter":name_filter} 
-        vm_resp = self._post_request("vms/list",payload)
+        vm_resp = self._get_request(f"vms/{vm_uuid}")
         vm = VM()
-        vm.load_entity(vm_resp['entities'][0])
+        vm.load_entity(vm_resp)
         return vm
     
     def subnet_by_name(self, sn_name, cluster_uuid):
@@ -248,7 +258,7 @@ class Prism_Central:
             r.raise_for_status()
             with open(local_filename, 'wb') as f:
                 pbar = tqdm(total=image_size, unit="B", unit_scale=True, unit_divisor=1024)
-                for chunk in r.iter_content(chunk_size=1048576):   # 1MB chunks
+                for chunk in r.iter_content(chunk_size=1024*1024*1024):   # 1GB chunks
                     f.write(chunk)
                     pbar.update(len(chunk))
         return local_filename
@@ -311,34 +321,36 @@ class Prism_Central:
         resp = self._post_request("images",image_create)
         return resp['status']['execution_context']['task_uuid']
 
-
     def poll_for_task(self, task_uuid):
         response = self._get_request(f"tasks/{task_uuid}")
         return response
 
 def wait_for_task(pc, task_uuid, poll_time=10):
-    print (f"Waiting for task: {task_uuid} to complete")
+    print (f" Waiting for task: {task_uuid} to complete")
     while True:
         res = pc.poll_for_task(task_uuid)
         perc = res['percentage_complete']
         if 'SUCCEEDED' in res['status']:
-            print(f" - {perc}% Complete")
+            print(f"   {perc}% Complete")
             return res
         elif 'FAILED' in res['status']:
             return None
         else:
-            print(f" - {perc}% Complete")
+            print(f"   {perc}% Complete")
             time.sleep(poll_time)
 
 def snapshot_vm(vm_uuid, pc):
     pass
     # Connect to PC and get VM's cluster UUID
     new_vm = pc.vm_by_uuid(vm_uuid)
-    pe_ip = pc['clusters'][new_vm['cluster_uuid']]['cvm_ips'][0] 
+    pe_ip = pc.clusters[new_vm.cluster_uuid]['cvm_ips'][0] 
+    pe_name = pc.clusters[new_vm.cluster_uuid]['name']
+
     # Connect to PE and take snapshot
     pe = Prism_Element(pe_ip,pe_user,pe_pass)
-    res = pe.snap_vm(vm_uuid,f"{new_vm['name']}_SNAP")
-    return res
+    snapshot_name = f"{new_vm.name}_SNAP_{pe_name}"
+    res = pe.snap_vm(vm_uuid,snapshot_name)
+    return (new_vm.name, snapshot_name, pe_name, res)
 
 
 if __name__ == "__main__":
@@ -356,13 +368,14 @@ if __name__ == "__main__":
     target_subnet_name = "Prod"
 
    # Gather info on the source VM
+    print()
     print(f"Gathering Information on source VM: {source_vm_name}")
     svm = source_pc.vm_by_name(source_vm_name)
 
     print(f"- VM uuid: {svm.uuid}")
     print(f"- Disk uuid: {svm.disk_uuid}")
 
-   
+    print() 
     print(f"Generating image from source VM")
     task_id = source_pc.generate_image(svm, source_image_name)
     completed_task = wait_for_task(source_pc, task_id)
@@ -373,10 +386,12 @@ if __name__ == "__main__":
     print(f"- Source Image uuid: {source_image_uuid}")
 
    # Download the image locally
-    print("Downloading Image to Local System") 
+    print()
+    print(f"Downloading Image to Local System - Filename: {local_image_filename}") 
     source_pc.download_image(source_image_uuid,local_image_filename)
 
    # Create our image at the target pc
+    print()
     print("Creating Image object in Target Prism Central")
     task_id = target_pc.create_image(source_image_name)
     completed_task = wait_for_task(target_pc, task_id)
@@ -384,10 +399,12 @@ if __name__ == "__main__":
     print(f"- Target Image uuid: {target_image_uuid}")
 
    # Upload our image to the target pc
+    print()
     print("Uploading Image to Target Prism Central")
     target_pc.upload_image(target_image_uuid,local_image_filename)
 
-   # Create our new VMs
+  # Create our new VMs
+    print()
     print("Creating VMs on all clusters attached to Source Prism Central")
     new_vm = VM()
     new_vm.description = "Generated VM from Distribution Script"
@@ -412,17 +429,23 @@ if __name__ == "__main__":
        source_tasks.append(task_id)
 
     source_vms = list()
+    print("Waiting for VM creations to finish")
     for task_uuid in source_tasks:
-       print(" - Waiting for VM creations to finish")
        resp = wait_for_task(source_pc,task_uuid) 
        source_vms.append(resp['entity_reference_list'][0]['uuid'])
 
-    print(" - Creating Snapshots on VMs")
+    output_info = list()
+    print()
+    print("Creating Snapshots on VMs")
     for vm in source_vms:
-        snapshot_vm(vm,source_pc)
+        res = snapshot_vm(vm,source_pc)
+        output_info.append(f"Cluster: {res[2]} VM: {res[0]} Snap: {res[1]}")
+    print(" - Completed Snapshots")
 
    # Create VMs on clusters attached to the target
    # We kick them off in parallel, then check them all at once for success
+    print()
+    print("Creating VMs on all clusters attached to Target Prism Central")
     target_tasks = list()
     for cluster_uuid in target_pc.clusters.keys():
        # We need to determine the subnets attached and connect to that subnet                 
@@ -430,7 +453,7 @@ if __name__ == "__main__":
        subnet_uuid = target_pc.subnet_by_name(target_subnet_name,cluster_uuid)
        new_vm.subnet = subnet_uuid
        new_vm.cluster_uuid = cluster_uuid
-       new_vm.disk_uuid = source_image_uuid
+       new_vm.disk_uuid = target_image_uuid
        new_vm.name = f"{source_vm_name}_VM_{target_pc.clusters[cluster_uuid]['name']}"
 
        print(f" - Creating VM {new_vm.name}")
@@ -438,13 +461,28 @@ if __name__ == "__main__":
        target_tasks.append(task_id)
 
     target_vms = list()
+    print("Waiting for VM creations to finish")
     for task_uuid in target_tasks:
-       print(" - Waiting for VM creations to finish")
        resp = wait_for_task(target_pc,task_uuid) 
        target_vms.append(resp['entity_reference_list'][0]['uuid'])
-
-    print(" - Creating Snapshots on VMs")
+ 
+    print()
+    print("Creating Snapshots on VMs")
     for vm in target_vms:
-        snapshot_vm(vm,target_pc)
+        res = snapshot_vm(vm,target_pc)
+        output_info.append(f"Cluster: {res[2]} VM: {res[0]} Snap: {res[1]}")
     
-    print("Complete")
+    print(" - Completed Snapshots")
+    print()
+    print("Cleaning up local image")
+    os.unlink(local_image_filename)
+
+    print()
+    print("Process Complete")
+    print(f"Souce VM: {source_vm_name}")
+    print(f"Image Name: {source_image_name}")
+    print(" Cluster Report ")
+    print(" -------------------------------")
+    for i in output_info:
+        print(f"   {i}")
+    print()
